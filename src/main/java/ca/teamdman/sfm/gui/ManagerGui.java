c36376c9537467e32dc0de0282292e04857ad7a8
TeamDman <TeamDman9201@gmail.com>
2019-08-06 23:34:49 -0400

    Cleanup

--- src/main/java/ca/teamdman/sfm/gui/ManagerGui.java
+++ src/main/java/ca/teamdman/sfm/gui/ManagerGui.java
@@ -3,8 +3,6 @@ package ca.teamdman.sfm.gui;
 import ca.teamdman.sfm.SFM;
 import ca.teamdman.sfm.container.ManagerContainer;
 import com.mojang.blaze3d.platform.GlStateManager;
-import com.sun.java.accessibility.util.java.awt.TextComponentTranslator;
-import net.java.games.input.Mouse;
 import net.minecraft.client.gui.IHasContainer;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.util.ResourceLocation;
@@ -13,8 +11,8 @@ import net.minecraft.util.text.TranslationTextComponent;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
+import static ca.teamdman.sfm.gui.MouseButton.LEFT;
 import static ca.teamdman.sfm.gui.Sprite.*;
 
 public class ManagerGui extends BaseGui implements IHasContainer<ManagerContainer> {
@@ -23,13 +21,17 @@ public class ManagerGui extends BaseGui implements IHasContainer<ManagerContaine
 	private static final ResourceLocation BACKGROUND_LEFT  = new ResourceLocation(SFM.MOD_ID, "textures/gui/background_1.png");
 	private static final ResourceLocation BACKGROUND_RIGHT = new ResourceLocation(SFM.MOD_ID, "textures/gui/background_2.png");
 	private final        List<FlowAction> actionList       = new ArrayList<>();
-	private FlowAction focusedAction = null;
+	private              FlowAction       focusedAction    = null;
+
 	{
 		actionList.add(new FlowAction(50, 50, INPUT, new TranslationTextComponent("woot"), () -> {
+			System.out.println("left");
 		}));
 		actionList.add(new FlowAction(150, 50, OUTPUT, new TranslationTextComponent("woot"), () -> {
+			System.out.println("center");
 		}));
 		actionList.add(new FlowAction(250, 50, INPUT, new TranslationTextComponent("woot"), () -> {
+			System.out.println("right");
 		}));
 	}
 
@@ -57,14 +59,14 @@ public class ManagerGui extends BaseGui implements IHasContainer<ManagerContaine
 	public boolean mouseClicked(double x, double y, int button) {
 		double sx = scaleX((float) x);
 		double sy = scaleY((float) y);
-//		super.mouseClicked(sx, sy, button);
-		if (button != MouseButton.LEFT.ordinal())
+		//		super.mouseClicked(sx, sy, button);
+		if (button != LEFT.ordinal())
 			return false;
 
 		int mx = (int) (sx - guiLeft);
 		int my = (int) (sy - guiTop);
 		for (FlowAction action : actionList) {
-			if (action.isInBounds(mx,my)) {
+			if (action.isInBounds(mx, my)) {
 				action.setPressed(true);
 				this.focusedAction = action;
 				return true;
@@ -75,7 +77,7 @@ public class ManagerGui extends BaseGui implements IHasContainer<ManagerContaine
 
 	@Override
 	public boolean mouseDragged(double x, double y, int button, double dx, double dy) {
-		if (button != MouseButton.LEFT.ordinal())
+		if (button != LEFT.ordinal())
 			return false;
 		if (focusedAction == null)
 			return false;
@@ -91,14 +93,14 @@ public class ManagerGui extends BaseGui implements IHasContainer<ManagerContaine
 
 	@Override
 	public boolean mouseReleased(double x, double y, int button) {
-		if (button != MouseButton.LEFT.ordinal())
+		if (button != LEFT.ordinal())
 			return false;
 		if (focusedAction == null)
 			return false;
 
 		int mx = scaleX(x) - guiLeft;
 		int my = scaleY(y) - guiTop;
-		if (focusedAction.isInBounds(mx,my))
+		if (focusedAction.isInBounds(mx, my))
 			focusedAction.click();
 
 		focusedAction.setPressed(false);
