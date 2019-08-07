79d328d319e0d513ba883da097ed2f185e056ac5
TeamDman <TeamDman9201@gmail.com>
2019-08-07 00:21:06 -0400

    Dragging and legend

--- src/main/java/ca/teamdman/sfm/gui/ManagerGui.java
+++ src/main/java/ca/teamdman/sfm/gui/ManagerGui.java
@@ -3,7 +3,13 @@ package ca.teamdman.sfm.gui;
 import ca.teamdman.sfm.SFM;
 import ca.teamdman.sfm.container.ManagerContainer;
 import com.mojang.blaze3d.platform.GlStateManager;
+import com.sun.jna.platform.KeyboardUtils;
+import net.java.games.input.Keyboard;
+import net.minecraft.client.KeyboardListener;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.gui.IHasContainer;
+import net.minecraft.client.resources.I18n;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.text.ITextComponent;
@@ -22,6 +28,9 @@ public class ManagerGui extends BaseGui implements IHasContainer<ManagerContaine
 	private static final ResourceLocation BACKGROUND_RIGHT = new ResourceLocation(SFM.MOD_ID, "textures/gui/background_2.png");
 	private final        List<FlowAction> actionList       = new ArrayList<>();
 	private              FlowAction       focusedAction    = null;
+	private boolean dragging = false;
+	private int dragOffsetX = 0;
+	private int dragOffsetY = 0;
 
 	{
 		actionList.add(new FlowAction(50, 50, INPUT, new TranslationTextComponent("woot"), () -> {
@@ -53,6 +62,13 @@ public class ManagerGui extends BaseGui implements IHasContainer<ManagerContaine
 		drawTexture(0, 0, 0, 0, 256, 256);
 		bindTexture(BACKGROUND_RIGHT);
 		drawTexture(256, 0, 0, 0, 256, 256);
+		drawRightAlignedString(
+				this.font,
+				I18n.format("gui.sfm.manager.legend.move"),
+				(int) fixScaledCoordinate(512, getScale(), minecraft.mainWindow.getWidth()) - 4,
+				(int) fixScaledCoordinate(256, getScale(), minecraft.mainWindow.getHeight())- 4,
+				16777215
+		);
 	}
 
 	@Override
@@ -67,7 +83,13 @@ public class ManagerGui extends BaseGui implements IHasContainer<ManagerContaine
 		int my = (int) (sy - guiTop);
 		for (FlowAction action : actionList) {
 			if (action.isInBounds(mx, my)) {
-				action.setPressed(true);
+				if (hasAltDown()) {
+					dragging = true;
+					dragOffsetX = action.getX() - mx;
+					dragOffsetY = action.getY() - my;
+				} else {
+					action.setPressed(true);
+				}
 				this.focusedAction = action;
 				return true;
 			}
@@ -77,22 +99,35 @@ public class ManagerGui extends BaseGui implements IHasContainer<ManagerContaine
 
 	@Override
 	public boolean mouseDragged(double x, double y, int button, double dx, double dy) {
-		if (button != LEFT.ordinal())
-			return false;
-		if (focusedAction == null)
-			return false;
-
 		int mx = scaleX(x) - guiLeft;
 		int my = scaleY(y) - guiTop;
-		if (focusedAction.isInBounds(mx, my) == focusedAction.isPressed())
-			return false;
+		if (dragging) {
+			if (!hasAltDown())
+				return false;
+			if (focusedAction == null)
+				return false;
+			if (hasShiftDown()) {
+				mx-=(mx+dragOffsetX)%10;
+				my-=(my+dragOffsetY)%10;
+			}
+			focusedAction.setX(mx + dragOffsetX);
+			focusedAction.setY(my + dragOffsetY);
+		} else {
+			if (button != LEFT.ordinal())
+				return false;
+			if (focusedAction == null)
+				return false;
+			if (focusedAction.isInBounds(mx, my) == focusedAction.isPressed())
+				return false;
 
-		focusedAction.setPressed(!focusedAction.isPressed());
+			focusedAction.setPressed(!focusedAction.isPressed());
+		}
 		return true;
 	}
 
 	@Override
 	public boolean mouseReleased(double x, double y, int button) {
+		dragging = false;
 		if (button != LEFT.ordinal())
 			return false;
 		if (focusedAction == null)
@@ -100,9 +135,10 @@ public class ManagerGui extends BaseGui implements IHasContainer<ManagerContaine
 
 		int mx = scaleX(x) - guiLeft;
 		int my = scaleY(y) - guiTop;
-		if (focusedAction.isInBounds(mx, my))
-			focusedAction.click();
+		if (!focusedAction.isInBounds(mx, my) || !focusedAction.isPressed())
+			return false;
 
+		focusedAction.click();
 		focusedAction.setPressed(false);
 		this.focusedAction = null;
 		return true;
