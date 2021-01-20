67e7395a92c7b3c2553208e59df24cb25d574dda
TeamDman <TeamDman9201@gmail.com>
2021-01-20 00:26:34 -0500

    Only tick executor on server, tick internally instead of worldtime

--- src/main/java/ca/teamdman/sfm/common/tile/manager/FlowExecutor.java
+++ src/main/java/ca/teamdman/sfm/common/tile/manager/FlowExecutor.java
@@ -1,6 +1,8 @@
 package ca.teamdman.sfm.common.tile.manager;
 
+import ca.teamdman.sfm.SFM;
 import ca.teamdman.sfm.common.flow.data.TimerTriggerFlowData;
+import ca.teamdman.sfm.common.util.SFMUtil;
 import java.util.ArrayDeque;
 import java.util.Deque;
 import java.util.List;
@@ -10,7 +12,7 @@ public class FlowExecutor {
 
 	private final ManagerTileEntity TILE;
 	private final Deque<ExecutionFrame> FRAMES = new ArrayDeque<>();
-	int count = 0;
+	private int tick = 0;
 
 	public FlowExecutor(ManagerTileEntity TILE) {
 		this.TILE = TILE;
@@ -18,13 +20,22 @@ public class FlowExecutor {
 
 	public void tick() {
 		World world = TILE.getWorld();
-		if (world == null) {
+		if (world == null || world.isRemote) {
 			return;
 		}
+		tick++;
+		ExecutionState state = new ExecutionState();
 		TILE.getFlowDataContainer().get(TimerTriggerFlowData.class)
-			.filter(t -> world.getGameTime() % t.interval == 0)
-			.map(t -> new ExecutionFrame(TILE, t))
+			.filter(t -> tick % t.interval == 0)
+			.map(t -> new ExecutionFrame(TILE, t, state))
 			.forEach(FRAMES::add);
+
+		if (!FRAMES.isEmpty()) {
+			SFM.LOGGER.debug(
+				SFMUtil.getMarker(getClass()),
+				"Executing trigger"
+			);
+		}
 
 		while (!FRAMES.isEmpty()) {
 			ExecutionFrame frame = FRAMES.pop();
