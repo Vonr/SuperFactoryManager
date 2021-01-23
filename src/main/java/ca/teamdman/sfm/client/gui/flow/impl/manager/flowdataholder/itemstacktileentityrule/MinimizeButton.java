c923af65ce859096a7c15b46870207c960c1216a
TeamDman <TeamDman9201@gmail.com>
2021-01-23 16:38:10 -0500

    Make minimize button use method to change visibility

--- src/main/java/ca/teamdman/sfm/client/gui/flow/impl/manager/flowdataholder/itemstacktileentityrule/MinimizeButton.java
+++ src/main/java/ca/teamdman/sfm/client/gui/flow/impl/manager/flowdataholder/itemstacktileentityrule/MinimizeButton.java
@@ -27,8 +27,7 @@ class MinimizeButton extends FlowMinusButton {
 
 	@Override
 	public void onClicked(int mx, int my, int button) {
-		PARENT.getData().open = false;
-		PARENT.CONTROLLER.SCREEN.sendFlowDataToServer(PARENT.getData());
+		PARENT.setVisible(false);
 	}
 
 	@Override
