41cbfce959061dc3b0bbc2b7e7d212cee156eda2
TeamDman <TeamDman9201@gmail.com>
2021-03-14 12:06:07 -0400

    Switch nbt serialization of blockpos to use more friendly format

--- src/main/java/ca/teamdman/sfm/common/util/BlockPosList.java
+++ src/main/java/ca/teamdman/sfm/common/util/BlockPosList.java
@@ -5,7 +5,7 @@ import java.util.Collection;
 import java.util.stream.IntStream;
 import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.nbt.ListNBT;
-import net.minecraft.nbt.LongNBT;
+import net.minecraft.nbt.NBTUtil;
 import net.minecraft.network.PacketBuffer;
 import net.minecraft.util.math.BlockPos;
 import net.minecraftforge.common.util.Constants.NBT;
@@ -24,17 +24,15 @@ public class BlockPosList extends ArrayList<BlockPos> {
 	}
 
 	public BlockPosList(CompoundNBT tag, String key) {
-		tag.getList(key, NBT.TAG_LONG).stream()
-			.map(LongNBT.class::cast)
-			.map(LongNBT::getLong)
-			.map(BlockPos::fromLong)
+		tag.getList(key, NBT.TAG_COMPOUND).stream()
+			.map(CompoundNBT.class::cast)
+			.map(NBTUtil::readBlockPos)
 			.forEach(this::add);
 	}
 
 	public ListNBT serialize() {
 		return stream()
-			.map(BlockPos::toLong)
-			.map(LongNBT::valueOf)
+			.map(NBTUtil::writeBlockPos)
 			.collect(ListNBT::new, ListNBT::add, ListNBT::addAll);
 	}
 
