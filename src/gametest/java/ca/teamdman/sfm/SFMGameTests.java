daa879958e469afadd95b09a9496e14d50eec79b
TeamDman <TeamDman9201@gmail.com>
2022-09-29 01:36:11 -0400

    Begin looking into game tests, move datagen to separate sourceset

---
+++ src/gametest/java/ca/teamdman/sfm/SFMGameTests.java
@@ -0,0 +1,13 @@
+package ca.teamdman.sfm;
+
+import net.minecraft.gametest.framework.GameTest;
+import net.minecraft.gametest.framework.GameTestHelper;
+import net.minecraftforge.gametest.GameTestHolder;
+
+@GameTestHolder(SFM.MOD_ID)
+public class SFMGameTests {
+    @GameTest(template = "myfirststructure")
+    public static void exampleTest(GameTestHelper helper) {
+
+    }
+}
