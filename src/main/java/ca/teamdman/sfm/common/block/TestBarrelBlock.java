42910b632ba68c1c5476d76ef501274f088ddb7f
TeamDman <TeamDman9201@gmail.com>
2023-04-15 14:51:35 -0400

    simplify test barrel destruction

--- src/main/java/ca/teamdman/sfm/common/block/TestBarrelBlock.java
+++ src/main/java/ca/teamdman/sfm/common/block/TestBarrelBlock.java
@@ -1,12 +1,9 @@
 package ca.teamdman.sfm.common.block;
 
 import net.minecraft.core.BlockPos;
-import net.minecraft.world.Container;
-import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.BarrelBlock;
 import net.minecraft.world.level.block.SoundType;
-import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.material.Material;
@@ -19,14 +16,7 @@ public class TestBarrelBlock extends BarrelBlock {
     @Override
     public void onRemove(BlockState pState, Level pLevel, BlockPos pPos, BlockState pNewState, boolean pIsMoving) {
         if (!pState.is(pNewState.getBlock())) {
-            BlockEntity blockentity = pLevel.getBlockEntity(pPos);
-            if (blockentity instanceof Container container) {
-                // void all items so they don't get dropped in the super call
-                // this prevents lag when rerunning tests that destroy and recreate all the barrels
-                for (int i = 0; i < container.getContainerSize(); i++) {
-                    container.setItem(i, ItemStack.EMPTY);
-                }
-            }
+            pLevel.removeBlockEntity(pPos);
             super.onRemove(pState, pLevel, pPos, pNewState, pIsMoving);
         }
     }
