25b6af539c877b31dcc391452a7f9d5c4eee2c48
TeamDman <TeamDman9201@gmail.com>
2023-04-26 00:15:31 -0400

    Add many lava cauldron test

--- src/gametest/java/ca/teamdman/sfm/compat/mekanism/SFMMekanismCompatGameTests.java
+++ src/gametest/java/ca/teamdman/sfm/compat/mekanism/SFMMekanismCompatGameTests.java
@@ -22,8 +22,14 @@ import net.minecraft.gametest.framework.GameTest;
 import net.minecraft.gametest.framework.GameTestHelper;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraftforge.common.capabilities.ForgeCapabilities;
+import net.minecraftforge.fluids.FluidStack;
 import net.minecraftforge.gametest.GameTestHolder;
 import net.minecraftforge.gametest.PrefixGameTestTemplate;
+
+import java.util.ArrayList;
+import java.util.Objects;
 
 @GameTestHolder(SFM.MOD_ID)
 @PrefixGameTestTemplate(false)
@@ -409,6 +415,76 @@ public class SFMMekanismCompatGameTests extends SFMGameTestBase {
                     "Contents did not arrive"
             );
             helper.succeed();
+        });
+    }
+
+
+    @GameTest(template = "25x3x25")
+    public static void mana_lava_cauldrons(GameTestHelper helper) {
+        // designate positions
+        var sourceBlocks = new ArrayList<BlockPos>();
+        var destBlocks = new ArrayList<BlockPos>();
+        var managerPos = new BlockPos(0, 2, 0);
+
+        // set up cauldrons
+        for (int x = 0; x < 25; x++) {
+            for (int z = 1; z < 25; z++) {
+                helper.setBlock(new BlockPos(x, 2, z), SFMBlocks.CABLE_BLOCK.get());
+                helper.setBlock(new BlockPos(x, 3, z), Blocks.LAVA_CAULDRON);
+                sourceBlocks.add(new BlockPos(x, 3, z));
+            }
+        }
+
+        // set up tanks
+        for (int i = 1; i < 25; i++) {
+            BlockPos tankPos = new BlockPos(i, 2, 0);
+            helper.setBlock(tankPos, MekanismBlocks.BASIC_FLUID_TANK.getBlock());
+            destBlocks.add(tankPos);
+        }
+
+        // set up the manager
+        helper.setBlock(managerPos, SFMBlocks.MANAGER_BLOCK.get());
+        ManagerBlockEntity manager = (ManagerBlockEntity) helper.getBlockEntity(managerPos);
+        manager.setItem(0, new ItemStack(SFMItems.DISK_ITEM.get()));
+
+        // create the program
+        var program = """
+                    NAME "many inventory lag test"
+                                    
+                    EVERY 20 TICKS DO
+                        INPUT fluid:*:* FROM source
+                        OUTPUT fluid:*:* TO dest TOP SIDE
+                    END
+                """;
+
+        // set the labels
+        sourceBlocks.forEach(pos -> SFMLabelNBTHelper.addLabel(
+                manager.getDisk().get(),
+                "source",
+                helper.absolutePos(pos)
+        ));
+        destBlocks.forEach(pos -> SFMLabelNBTHelper.addLabel(manager.getDisk().get(), "dest", helper.absolutePos(pos)));
+
+        // load the program
+        manager.setProgram(program);
+        succeedIfManagerDidThingWithoutLagging(helper, manager, () -> {
+            sourceBlocks.forEach(pos -> helper.assertBlock(
+                    pos,
+                    Blocks.CAULDRON::equals,
+                    () -> "Cauldron did not empty"
+            ));
+            int found = destBlocks
+                    .stream()
+                    .map(helper::getBlockEntity)
+                    .map(be -> be.getCapability(ForgeCapabilities.FLUID_HANDLER))
+                    .map(x -> x.orElse(null))
+                    .peek(Objects::requireNonNull)
+                    .map(x -> x.getFluidInTank(0))
+                    .mapToInt(FluidStack::getAmount)
+                    .sum();
+            assertTrue(found == 1000 * 25 * 24, "Not all fluids were moved (found " + found + ")");
+            helper.succeed();
+
         });
     }
 }
