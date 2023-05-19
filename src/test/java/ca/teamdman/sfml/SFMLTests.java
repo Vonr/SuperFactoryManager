2fa9bacc3a6d83cede2f49e7a7a9229b44fd20a1
TeamDman <TeamDman9201@gmail.com>
2023-05-18 23:05:13 -0400

    suppress generic warnings in test

--- src/test/java/ca/teamdman/sfml/SFMLTests.java
+++ src/test/java/ca/teamdman/sfml/SFMLTests.java
@@ -24,6 +24,7 @@ import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SuppressWarnings("unchecked")
 public class SFMLTests {
     public ArrayList<String> getCompileErrors(String input) {
         var lexer = new SFMLLexer(CharStreams.fromString(input));
