e8a5264e47851cb51e02d0020dd329f27b576eeb
TeamDman <TeamDman9201@gmail.com>
2023-10-10 23:23:49 -0400

    camera following dude

--- ai/games/cursor-hero/src/main.rs
+++ ai/games/cursor-hero/src/main.rs
@@ -1,11 +1,12 @@
 use bevy::prelude::*;
-
+use bevy_inspector_egui::quick::WorldInspectorPlugin;
+use bevy::input::common_conditions::input_toggle_active;
 
 mod screen_backgrounds;
 use screen_backgrounds::ScreenBackgroundsPlugin;
 
 mod cursor_character;
-use cursor_character::CursorCharacterPlugin;
+use cursor_character::{CursorCharacterPlugin, Character};
 
 fn main() {
     App::new()
@@ -16,15 +17,17 @@ fn main() {
                     primary_window: Some(Window {
                         title: "Cursor Hero".into(),
                         resolution: (640.0, 480.0).into(),
-                        resizable: false,
+                        resizable: true,
                         ..default()
                     }),
                     ..default()
                 })
                 .build(),
         )
+        .add_plugins(WorldInspectorPlugin::default().run_if(input_toggle_active(false, KeyCode::Grave)))
         .add_plugins((ScreenBackgroundsPlugin, CursorCharacterPlugin))
         .add_systems(Startup, setup)
+        .add_systems(Update, camera_follow_tick)
         .run();
 }
 
@@ -32,4 +35,17 @@ fn setup(
     mut commands: Commands,
 ) {
     commands.spawn(Camera2dBundle::default());
+}
+
+
+fn camera_follow_tick(
+    mut set: ParamSet<(
+        Query<&mut Transform, With<Camera>>,
+        Query<(&mut Transform, With<Character>)>,
+    )>,
+) {
+    let char_pos = set.p1().single().0.translation;
+    let mut camera = set.p0();
+    let mut camera = camera.single_mut();
+    camera.translation = char_pos;
 }
