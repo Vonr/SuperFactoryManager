4c963a3fd87ac8872ef4b218b51e60396fbedf14
TeamDman <TeamDman9201@gmail.com>
2023-10-10 23:54:02 -0400

    screen refreshing but laggy

--- ai/games/cursor-hero/src/screen_backgrounds.rs
+++ ai/games/cursor-hero/src/screen_backgrounds.rs
@@ -13,7 +13,10 @@ impl Plugin for ScreenBackgroundsPlugin {
 
 #[derive(Component, Default, Reflect)]
 #[reflect(Component)]
-pub struct Screen {}
+pub struct Screen {
+    id: u32,
+    refresh_rate: Timer,
+}
 
 #[derive(Component)]
 pub struct ScreenParent;
@@ -38,47 +41,35 @@ fn spawn_screens(
         commands.spawn((
             SpriteBundle {
                 texture,
-                transform: Transform::from_xyz(0.0, 0.0, -1.0), // Position behind the character
+                transform: Transform::from_xyz(screen.display_info.x as f32, screen.display_info.y as f32, -1.0), // Position behind the character
                 ..Default::default()
             },
-            Screen {},
+            Screen {
+                id: screen.display_info.id,
+                refresh_rate: Timer::from_seconds(1.0, TimerMode::Repeating)
+            },
             Name::new(format!("Screen {}", screen.display_info.id)),
         ));
     }
-    
-    // // Capture the screen
-    // let screen = ScreenLib::all().unwrap()[0];
-    // let image_buf = screen.capture().unwrap();
-    // let dynamic_image = DynamicImage::ImageRgba8(image_buf);
-    // // Create an image using Bevy's Image type
-    // let image = Image::from_dynamic(dynamic_image, true);
-    // let texture = textures.add(image);
-
-    // // Create a sprite with the background texture
-    // commands.spawn((
-    //     SpriteBundle {
-    //         texture,
-    //         transform: Transform::from_xyz(0.0, 0.0, -1.0), // Position behind the character
-    //         ..Default::default()
-    //     },
-    //     Screen {
-
-    //     },
-    // ));
 }
 
 fn update_screens(
-    mut commands: Commands,
-    mut query: Query<(Entity, &mut Screen, &mut Transform), Changed<Screen>>,
-    // mut parent_query: Query<&mut Transform, With<ScreenParent>>,
+    mut query: Query<(&mut Screen, &Handle<Image>)>,
+    mut textures: ResMut<Assets<Image>>,
+    time: Res<Time>,
 ) {
-    // for (entity, mut screen, mut transform) in query.iter_mut() {
-    //     let mut parent_transform = parent_query.single_mut().unwrap();
-    //     let screen_transform = Transform::from_translation(Vec3::new(screen.position.x, screen.position.y, 0.0));
-    //     let screen_transform = parent_transform.compute_matrix() * screen_transform.compute_matrix();
-    //     transform.translation = screen_transform.translation;
-    //     transform.scale = screen_transform.scale;
-    //     transform.rotation = screen_transform.rotation;
-    //     commands.entity(entity).remove::<Changed<Screen>>();
-    // }
+    for (mut screen, texture) in &mut query {
+        screen.refresh_rate.tick(time.delta());
+        if screen.refresh_rate.finished() {
+            for libscreen in ScreenLib::all().unwrap().iter() {
+                if libscreen.display_info.id == screen.id {
+                    let image_buf = libscreen.capture().unwrap();
+                    let dynamic_image = DynamicImage::ImageRgba8(image_buf);
+                    let image = Image::from_dynamic(dynamic_image, true);
+
+                    textures.get_mut(&texture).unwrap().data = image.data;
+                }
+            }
+        }
+    }
 }
