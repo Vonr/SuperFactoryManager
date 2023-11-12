4fda290f1ee60a7ec798291f996ea8621e45bafd
TeamDman <TeamDman9201@gmail.com>
2023-11-12 13:55:51 -0500

    fix label gun not swinging on use, pretty build.gradle alignment

--- src/main/java/ca/teamdman/sfm/common/item/LabelGunItem.java
+++ src/main/java/ca/teamdman/sfm/common/item/LabelGunItem.java
@@ -71,6 +71,7 @@ public class LabelGunItem extends Item {
                     Screen.hasControlDown(),
                     ctx.getPlayer().isShiftKeyDown()
             ));
+            return InteractionResult.SUCCESS;
         }
         return InteractionResult.CONSUME;
     }
