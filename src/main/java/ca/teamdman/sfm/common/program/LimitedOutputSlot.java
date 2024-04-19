7c1eadb60673807597f15884ae28a10f56535328
TeamDman <TeamDman9201@gmail.com>
2024-04-19 00:01:44 -0400

    validate slot as part of done check
    fixes #116

--- src/main/java/ca/teamdman/sfm/common/program/LimitedOutputSlot.java
+++ src/main/java/ca/teamdman/sfm/common/program/LimitedOutputSlot.java
@@ -8,25 +8,29 @@ public class LimitedOutputSlot<STACK, ITEM, CAP> {
     @SuppressWarnings("NotNullFieldNotInitialized") // done in init method in constructor
     public ResourceType<STACK, ITEM, CAP> type;
     @SuppressWarnings("NotNullFieldNotInitialized") // done in init method in constructor
-    public CAP capability;
+    public CAP handler;
     public int slot;
     @SuppressWarnings("NotNullFieldNotInitialized") // done in init method in constructor
     public OutputResourceTracker<STACK, ITEM, CAP> tracker;
     private @Nullable STACK stackInSlotCache = null;
 
     public LimitedOutputSlot(
-            CAP capability, int slot, OutputResourceTracker<STACK, ITEM, CAP> tracker
+            CAP handler, int slot, OutputResourceTracker<STACK, ITEM, CAP> tracker
     ) {
-        this.init(capability, slot, tracker);
+        this.init(handler, slot, tracker);
     }
 
     public boolean isDone() {
         if (tracker.isDone()) {
             return true;
         }
+        if (slot > type.getSlots(handler)) {
+            // composter block changes how many slots it has between insertions
+            return true;
+        }
         STACK stack = getStackInSlot();
         long count = type.getAmount(stack);
-        if (count >= type.getMaxStackSize(capability, slot)) {
+        if (count >= type.getMaxStackSize(handler, slot)) {
             // if the maxStackSize is different, that will be handled by moveTo
             // for the general case, it will be faster to just assume 64 is the max stack size
             return true;
@@ -36,19 +40,19 @@ public class LimitedOutputSlot<STACK, ITEM, CAP> {
 
     public STACK getStackInSlot() {
         if (stackInSlotCache == null) {
-            stackInSlotCache = type.getStackInSlot(capability, slot);
+            stackInSlotCache = type.getStackInSlot(handler, slot);
         }
         return stackInSlotCache;
     }
 
     public STACK insert(STACK stack, boolean simulate) {
         if (!simulate) stackInSlotCache = null;
-        return type.insert(capability, slot, stack, simulate);
+        return type.insert(handler, slot, stack, simulate);
     }
 
     public void init(CAP handler, int slot, OutputResourceTracker<STACK, ITEM, CAP> tracker) {
         this.stackInSlotCache = null;
-        this.capability = handler;
+        this.handler = handler;
         this.tracker = tracker;
         this.slot = slot;
         //noinspection DataFlowIssue
