7f4bb4c645ff832c707ecd40858bbf5522871812
TeamDman <TeamDman9201@gmail.com>
2024-06-14 00:06:57 -0400

    free input statements in reverse order to satisfy the math

--- src/main/java/ca/teamdman/sfm/common/program/ProgramContext.java
+++ src/main/java/ca/teamdman/sfm/common/program/ProgramContext.java
@@ -111,8 +111,13 @@ public class ProgramContext {
         return REDSTONE_PULSES;
     }
 
+    /**
+     * We free in reverse order because the {@link InputStatement#inputCheck} needs LIFO ordering for the math to work
+     */
     public void free() {
-        getInputs().forEach(InputStatement::freeSlots);
+        for (int i = INPUTS.size() - 1; i >= 0; i--) {
+            INPUTS.get(i).freeSlots();
+        }
     }
 
     public enum ExecutionPolicy {
