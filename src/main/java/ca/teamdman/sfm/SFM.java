f0de0f5223538ae42942757b93f81a4c96f3a7fa
TeamDman <TeamDman9201@gmail.com>
2024-08-25 22:01:48 -0400

    remove test attachment thing from when I tried to move test barrel out of main src set

--- src/main/java/ca/teamdman/sfm/SFM.java
+++ src/main/java/ca/teamdman/sfm/SFM.java
@@ -3,7 +3,6 @@ package ca.teamdman.sfm;
 import ca.teamdman.sfm.client.registry.SFMMenuScreens;
 import ca.teamdman.sfm.common.SFMConfig;
 import ca.teamdman.sfm.common.registry.*;
-import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
@@ -12,15 +11,10 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.function.Consumer;
-
 @Mod("sfm")
 public class SFM {
     public static final String MOD_ID = "sfm";
     public static final Logger LOGGER = LogManager.getLogger(SFM.MOD_ID);
-    public static final List<Consumer<IEventBus>> TEST_ATTACHMENTS = new ArrayList<>();
     public SFM() {
         var bus = FMLJavaModLoadingContext
                 .get()
@@ -33,7 +27,6 @@ public class SFM {
         SFMMenus.register(bus);
         SFMRecipeTypes.register(bus);
         SFMRecipeSerializers.register(bus);
-        TEST_ATTACHMENTS.forEach(c -> c.accept(bus));
         SFMConfig.register(ModLoadingContext.get());
         bus.addListener((FMLClientSetupEvent e) -> SFMMenuScreens.register());
         bus.addListener((FMLCommonSetupEvent e) -> SFMPackets.register());
