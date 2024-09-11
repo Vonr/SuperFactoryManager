35fd7a86ea0a41b6c5c53791c80672833e51f652
Titop54 <jose22343@hotmail.com>
2024-09-11 12:31:07 +0200

    First try of the error checking (should be an option for low tier pc, because it can take some time to process)

--- vscodeextension/super-factory-manager-language/src/antlrg4/Parser.ts
+++ vscodeextension/super-factory-manager-language/src/antlrg4/Parser.ts
@@ -1,33 +1,95 @@
 /*
- * Class for parser and error checking
+ * File for parser and error checking
  */
-
+import * as vscode from 'vscode';
 import { CharStreams, CommonTokenStream } from 'antlr4ts';
 import { SFMLLexer } from '../generated/SFMLLexer';
 import { SFMLParser } from '../generated/SFMLParser';
+import { ANTLRErrorListener, RecognitionException, Recognizer } from 'antlr4ts';
 
-function parseInput(input: string) {
-    
+export function parseInput(input: string): { success: boolean, errors: any[] } {
     const inputStream = CharStreams.fromString(input);
     const lexer = new SFMLLexer(inputStream);
     const tokenStream = new CommonTokenStream(lexer);
     const parser = new SFMLParser(tokenStream);
-    const tree = parser.program();
-    if(parser.numberOfSyntaxErrors > 0) 
-    {
-        return false;
-    }
-    return true;
 
-    /*vscode.workspace.onDidSaveTextDocument((document) => {
-        const inputText = document.getText();
-        const isValid = parseInput(inputText);
+    const errorListener = new UnderlineErrorListener();
+    parser.removeErrorListeners();
+    parser.addErrorListener(errorListener);
+
+    const tree = parser.program();
+
+    const errors = errorListener.getErrors();
+
+    return {
+        success: parser.numberOfSyntaxErrors === 0,
+        errors: errors.map(error => {
+            return {
+                lineStart: error.lineStart,  // Línea de inicio donde ocurre el error
+                columnStart: error.columnStart,  // Columna de inicio del error
+                lineEnd: error.lineEnd,  // Línea de fin (puede ser igual a lineStart si es en una sola línea)
+                columnEnd: error.columnEnd,  // Columna de fin del error
+                message: error.message  // Mensaje de error
+            };
+        })
+    };
+}
+
+
+export class UnderlineErrorListener implements ANTLRErrorListener<any> {
+    private errors: any[] = [];
+    //Something ANTLR ask for, idk why
+    syntaxError<T>(
+        recognizer: Recognizer<T, any>,
+        offendingSymbol: T,
+        lineStart: number,
+        columnStart: number,
+        msg: string,
+        e: RecognitionException | undefined
+    ): void {
         
-        if (!isValid) {
-            //vscode.window.showErrorMessage('No valid');
-        } else {
-            //vscode.window.showInformationMessage('Valid');
-        }
-    });
-    return false;*/
+        const offendingToken = e?.getOffendingToken();
+        
+        const lineEnd = offendingToken ? offendingToken.line : lineStart; // Asegurar que 'lineEnd' no sea undefined
+        const columnEnd = offendingToken ? offendingToken.charPositionInLine + (offendingToken.text?.length || 0) : columnStart;
+
+        this.errors.push({
+            lineStart: lineStart,
+            columnStart: columnStart,
+            lineEnd: lineEnd,
+            columnEnd: columnEnd,
+            message: msg
+        });
+    }
+
+    getErrors() {
+        return this.errors;
+    }
+}
+
+export function handleDocument(document: vscode.TextDocument) {
+    const text = document.getText();
+    const diagnostics: vscode.Diagnostic[] = [];
+
+    const parseResult = parseInput(text);
+    const { success, errors } = parseResult;
+
+    if (!success) {
+        errors.forEach((error: any) => {
+            const { lineStart, columnStart, lineEnd, columnEnd, message } = error;
+
+            //From start to finish, dont work quite yet
+            const range = new vscode.Range(
+                new vscode.Position(lineStart - 1, 0),  // Start of the line
+                new vscode.Position(lineEnd - 1, columnEnd)  // End of the line
+            );
+
+            // Crear un diagnóstico y asociarlo al rango
+            const diagnostic = new vscode.Diagnostic(range, message, vscode.DiagnosticSeverity.Error);
+            diagnostics.push(diagnostic);
+        });
+    }
+
+    const diagnosticCollection = vscode.languages.createDiagnosticCollection('syntaxErrors');
+    diagnosticCollection.set(document.uri, diagnostics);
 }
--- vscodeextension/super-factory-manager-language/src/extension.ts
+++ vscodeextension/super-factory-manager-language/src/extension.ts
@@ -1,6 +1,7 @@
 import * as vscode from 'vscode';
 import { everySnippet } from './snippets/snippetController';
 import { activityBar, deleteTempFiles } from './activitybar/ActivityBar';
+import { handleDocument} from './antlrg4/Parser';
 
 /**
  * Main method to call everything we need
@@ -10,6 +11,11 @@ export function activate(context: vscode.ExtensionContext) {
     console.log("bruh activating");
     everySnippet(context);
     activityBar(context);
+    
+    const disposable = vscode.workspace.onDidSaveTextDocument((document) => {
+        handleDocument(document);
+    });
+    context.subscriptions.push(disposable);
 }
 
 export function deactivate() {
