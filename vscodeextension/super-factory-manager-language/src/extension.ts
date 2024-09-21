51e5c0ca2651281c0af3392b0118ab5ef975bb5f
Titop54 <jose22343@hotmail.com>
2024-09-21 17:14:39 +0200

    Snippet completion
    Now 2 more settings, one for enable/disable snippets and another for using a keyword or character to enable the snippets
    
    Without one, it should behave like if we use sfml.code-snippets more or less, except if you put ev, it wont show the snippet with other options for example

--- vscodeextension/super-factory-manager-language/src/extension.ts
+++ vscodeextension/super-factory-manager-language/src/extension.ts
@@ -1,5 +1,5 @@
 import * as vscode from 'vscode';
-import { everySnippet } from './snippets/snippetController';
+import { registerSnippets } from './snippets/snippetController';
 import { activityBar, deleteTempFiles } from './activitybar/ActivityBar';
 import { handleDocument} from './antlrg4/Parser';
 import { checkInputOutput } from './antlrg4/Warning';
@@ -10,7 +10,7 @@ import { checkInputOutput } from './antlrg4/Warning';
  */
 export function activate(context: vscode.ExtensionContext) {
     console.log("bruh activating");
-    everySnippet(context);
+    registerSnippets(context);
     activityBar(context);
     
     const checking = vscode.workspace.onDidSaveTextDocument((document) => {
