8b6f69484250c63a271fc76f0bd190bc7e5b2efa
TeamDman <TeamDman9201@gmail.com>
2026-06-20 23:19:10 -0400

    idea to open minecraft dir instead of repo root

--- platform/pwsh/open-idea.ps1
+++ platform/pwsh/open-idea.ps1
@@ -1,1 +1,1 @@
-git worktree list --porcelain | ? { $_.StartsWith("worktree") } | % { ($_.Split("worktree"))[1].Trim() } | Select-Object -SkipLast 1 | % { echo "$_"; idea $_; Start-Sleep -Seconds 5 }
+git worktree list --porcelain | ? { $_.StartsWith("worktree") } | % { ($_.Split("worktree"))[1].Trim() } | Select-Object -SkipLast 1 | % { echo "$_"; idea "$_\platform\minecraft"; Start-Sleep -Seconds 5 }
