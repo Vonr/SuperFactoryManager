b3cb5386ae1e97646bd5b4356512b7cacea4a57f
TeamDman <TeamDman9201@gmail.com>
2026-07-13 23:36:51 -0400

    build: exclude unavailable CC integrations

--- platform/minecraft/src/main/java/ca/teamdman/sfm/common/compat/computercraft/ComputerCraftIntegration.java
+++ platform/minecraft/src/main/java/ca/teamdman/sfm/common/compat/computercraft/ComputerCraftIntegration.java
@@ -1,11 +1,17 @@
 package ca.teamdman.sfm.common.compat.computercraft;
 
+import ca.teamdman.sfm.SFM;
+import ca.teamdman.sfm.common.compat.SFMModCompat;
 import dan200.computercraft.api.ForgeComputerCraftAPI;
 import dan200.computercraft.api.detail.VanillaDetailRegistries;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 
 /**
  * Registers SFM's public CC:Tweaked integration points once CC:Tweaked is known to be loaded.
  */
+@Mod.EventBusSubscriber(modid = SFM.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public final class ComputerCraftIntegration {
     private static boolean registered;
 
@@ -19,5 +25,12 @@ public final class ComputerCraftIntegration {
         ForgeComputerCraftAPI.registerPeripheralProvider(new SFMNetworkPeripheralProvider());
         VanillaDetailRegistries.ITEM_STACK.addProvider(new SFMItemDetailProvider());
         registered = true;
+    }
+
+    @SubscribeEvent
+    public static void onCommonSetup(FMLCommonSetupEvent event) {
+        if (SFMModCompat.isComputerCraftLoaded()) {
+            event.enqueueWork(ComputerCraftIntegration::register);
+        }
     }
 }
